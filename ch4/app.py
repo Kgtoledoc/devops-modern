49f60d22893762e3887af296fe495b1d6e5a1e8a
Kevin Giorgio Toledo De la cruz <ktoledo@bancodebogota.com.co>
2022-02-06 11:53:42 -0500

    Add the proxy address

--- ch4/app.py
+++ ch4/app.py
@@ -4,7 +4,7 @@ from flask import Flask
 from datetime import datetime
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis',port=6379)
+cache = redis.Redis(host='localhost',port=6379)
 
 def get_last_visited():
     try:
