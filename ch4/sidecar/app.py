0caff100e6f2e11cb260c669088d9aae7eb3a31a
Kevin Giorgio Toledo De la cruz <ktoledo@bancodebogota.com.co>
2022-02-06 22:51:19 -0500

    Changes in the app dockerfile

---
+++ ch4/sidecar/app.py
@@ -0,0 +1,20 @@
+import time
+import redis
+from flask import Flask
+from datetime import datetime
+
+app = Flask(__name__)
+cache = redis.Redis(host='localhost', port=6379)
+
+def get_secret():
+    try:
+        secret = cache.get('secret')
+        return secret
+    except redis.exceptions.ConnectionError as e:
+        raise e
+
+@app.route('/')
+def index():
+    secret = str(get_secret().decode('utf-8'))
+    return 'Hi There! The secrets is {}.\n'.format(secret)
+
